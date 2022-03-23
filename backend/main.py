2722000f8d2be561e45c8cdc507ce866d5076183
dionvitor <dionvictor11@gmail.com>
2022-03-22 22:22:16 -0300

    Adicionado estrutura inicial do backend com fastapi e docker.

---
+++ backend/main.py
@@ -0,0 +1,47 @@
+from typing import Dict
+
+from fastapi import FastAPI, Request
+from fastapi.openapi.utils import get_openapi
+from dotenv import load_dotenv
+
+from controllers.forum_controllers import forum_router
+
+
+app = FastAPI()
+app.include_router(forum_router)
+
+
+@app.get("/", include_in_schema=False)
+def api_root():
+    return {"message": "MyUni API"}
+
+
+@app.middleware("http")
+async def before_request(request: Request, call_next):
+    load_dotenv()
+
+    response = await call_next(request)
+    return response
+
+
+def customize_openapi(openapi_schema: Dict):
+    pass
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title="MyUni API",
+        version="1.0.0",
+        description="Endpoint documentation to MyUni api.",
+        routes=app.routes,
+    )
+
+    customize_openapi(openapi_schema)
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
