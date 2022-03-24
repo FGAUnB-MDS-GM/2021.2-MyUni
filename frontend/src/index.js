f837ed58fc390b83540e4f09b4ae5ac4156b89d1
emersonteles <emersonteles21@gmail.com>
2022-03-24 12:31:53 -0300

    feat: router to login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import Login from "./pages/login";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />}></Route>
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
