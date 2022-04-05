3bc1a38e7deb6d1628c140824bf536f1f92e96b9
Bernardo Pissutti <bernardopissutti@gmail.com>
2022-04-05 19:12:18 -0300

    Registration update

--- frontend/src/components/formRegistration/index.js
+++ frontend/src/components/formRegistration/index.js
@@ -11,19 +11,26 @@ function FormRegistration() {
       <form className="formRegistration">
         <h1 className="formRegistration_title">Faça seu Cadastro!</h1>
         <div className="formRegistration_group">
-          <Input type="text" title="Nome:" placeholder="Nome Completo" />
+          <div className="formRegistration_row">
+            <Input type="text" title="Nome:" placeholder="José" />
+            <Input type="text" title="Sobrenome:" placeholder="Silva" />
+          </div>
           <Input type="text" title="Matricula:" placeholder="193239092" />
+          <Input type="email" title="Email:" placeholder="aluno@aluno.com" />
           <select name="Curso">
             <option value="valor1">Valor 1</option>
             <option value="valor2" selected>Valor 2</option>
             <option value="valor3">Valor 3</option>
-          </select>
-          <Input type="password" title="Senha:" placeholder="********" />
+          </select> 
+          <div className = "formRegistration_row" >
+            <Input type="password" title="Senha:" placeholder="********" />
+            <Input type="password" title="Confirmar senha:" placeholder="********" />
+          </div>
         </div>
         <div className="formRegistration_group">
           <Button label="Cadastrar" onClick={onSubmit} />
           <a href="/login" className="formRegistration_signup">
-            <p>Clique aqui para voltar para o login</p>
+            <p>ja tem uma conta? <br /> Faça login</p>
           </a>
         </div>
       </form>
