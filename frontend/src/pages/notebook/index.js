d3ef5225fe0df1bbad6fc4962c39e4147c30a236
Bernardo Pissutti <bernardopissutti@gmail.com>
2022-04-21 20:14:18 -0300

    Tela de cadastro de pergunta e notebook

--- frontend/src/pages/notebook/index.js
+++ frontend/src/pages/notebook/index.js
@@ -1,49 +1,11 @@
 import Layout from "../../components/layout";
 import SearchInput from "../../components/searchInput";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
 import "./styles.scss";
 
 function Notebook() {
     const notebookTopic = [
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de Calculo 1 do dia 27/07/2019",
-            label: "Calculo 1",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de DIAC do dia 27/07/2019",
-            label: "DIAC",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de Calculo 1 do dia 27/07/2019",
-            label: "Calculo 1",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de DIAC do dia 27/07/2019",
-            label: "DIAC",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de Calculo 1 do dia 27/07/2019",
-            label: "Calculo 1",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de DIAC do dia 27/07/2019",
-            label: "DIAC",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de Calculo 1 do dia 27/07/2019",
-            label: "Calculo 1",
-        },
-        {
-            title: "Anotações da aula",
-            description: "Conteúdo de DIAC do dia 27/07/2019",
-            label: "DIAC",
-        },
         {
             title: "Anotações da aula",
             description: "Conteúdo de Calculo 1 do dia 27/07/2019",
@@ -88,12 +50,34 @@ function Notebook() {
                     })}
                 </div>
                 <div className="notebook_aside">
+                    <form className="notebook_aside_form">
+                        <h3>Adicionar um notebook</h3>
+                        <Input
+                        name="titulo"
+                        type="text"
+                        title="Titulo:"
+                        placeholder="titulo"
+                        />
+                        <Input
+                        name="descrição"
+                        type="text"
+                        title="Descrição:"
+                        placeholder="descrição"
+                        />
+                        <Input
+                        name="topico"
+                        type="text"
+                        title="Tópico:"
+                        placeholder="topico"
+                        />
+                        <div className="notebook_aside_form_button"><Button label="Adicionar" /></div>
+                    </form>
                     <aside className="notebook_aside_sidebar">
                         <h3>Tópicos</h3>
                         {notebookTopic.map((topic) => {
                             return (
                                 <ul>
-                                    <li>{topic.label}</li>
+                                    <li><a href="">{topic.label}</a></li>
                                 </ul>
                             );
                         })}
