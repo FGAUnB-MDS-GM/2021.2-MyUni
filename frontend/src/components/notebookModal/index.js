50627324721b923c8451f0b9f0618c0576e0e450
dev-brito <niguiluvi26@>
2022-04-27 00:51:37 -0300

    Getting dinamic data on notebook modal

--- frontend/src/components/notebookModal/index.js
+++ frontend/src/components/notebookModal/index.js
@@ -1,27 +1,32 @@
-import { useImperativeHandle, forwardRef, useState } from "react";
+import {useImperativeHandle, forwardRef, useState, useEffect} from "react";
 import { XIcon } from "@heroicons/react/outline";
 import Modal from "../modal";
 import Input from "../Input";
 import "./styles.scss";
+import Button from "../Button";
 
 function NotebookModal({ notebook }, ref) {
-  const defaultInputValues = {
-    title: "",
-    note: "",
-  };
   const [notebookModal, setNotebookModal] = useState(false);
-  const [values, setValues] = useState(defaultInputValues);
+  const [values, setValues] = useState(null);
+  const [edited, setEdited] = useState(false)
   function handleChange(event) {
     const { name, value } = event.target;
 
     setValues({ ...values, [name]: value });
+    setEdited(true);
   }
   useImperativeHandle(ref, () => ({
-    handleOpenModal: () => {
+    handleOpenModal: (notebook) => {
       console.log("imperative");
+      setValues(notebook)
+        console.log(notebook)
       setNotebookModal(true);
     },
   }));
+
+  useEffect(()=>{
+
+  }, [values])
 
   if (!notebookModal) return null;
 
@@ -41,7 +46,7 @@ function NotebookModal({ notebook }, ref) {
                 name="title"
                 type="textarea"
                 placeholder="Título"
-                value={values.title}
+                value={values?.title}
                 onChange={(event) => handleChange(event)}
                 style={{
                   background: "#fff",
@@ -55,10 +60,10 @@ function NotebookModal({ notebook }, ref) {
             </header>
             <main className="notebookModal_content_main">
               <Input
-                name="note"
+                name="description"
                 type="textarea"
                 placeholder="Nota"
-                value={values.note}
+                value={values?.description}
                 onChange={(event) => handleChange(event)}
                 style={{
                   background: "#fff",
@@ -71,6 +76,11 @@ function NotebookModal({ notebook }, ref) {
             </main>
           </div>
         </article>
+          {edited ?
+              <div className="notebookModal_confirm_button">
+                  <Button label="Confirmar Edição de Nota"/>
+              </div>
+              : null}
       </div>
     </Modal>
   );
